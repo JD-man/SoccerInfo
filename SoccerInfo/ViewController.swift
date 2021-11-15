093b631b6984f41e2967a7f37d8002736898db5e
JD-man <ekgkzmtm@gmail.com>
2021-11-15 17:35:53 +0900

    Project, TeamBuilding, ProjectPlan commit

---
+++ SoccerInfo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SoccerInfo
+//
+//  Created by JD_MacMini on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
+
