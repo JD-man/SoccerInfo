5d2fa2df4cc0b2f12eefc009734f5833b7e5508e
JD-man <ekgkzmtm@gmail.com>
2022-03-24 14:04:36 +0900

    Refactor: NewsVC to Code-based UI.

--- SoccerInfo/Views/News/NewsTableViewCell.swift
+++ SoccerInfo/Views/News/NewsTableViewCell.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 import Kingfisher
+import SnapKit
 
 final class NewsTableViewCell: UITableViewCell {
-    @IBOutlet weak var newsImageView: UIImageView!
-    @IBOutlet weak var newsTitleLabel: UILabel!
-    @IBOutlet weak var newsDescriptionLabel: UILabel!
+    private let newsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
     
-    @IBOutlet weak var newsImageViewWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var newsImageViewLeftConstraint: NSLayoutConstraint!
+    private let newsTitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        return label
+    }()
     
-    private var newsImageInitialWidth: CGFloat = 0
-    private var newsImageInitialLeftConstant: CGFloat = 0
+    private let newsDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 3
+        label.textColor = .systemGray2
+        label.font = .systemFont(ofSize: 13, weight: .medium)
+        return label
+    }()
+    
+    private var newsImageInitialWidth: CGFloat = 120
+    private var newsImageInitialLeftConstant: CGFloat = 15
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,27 +43,67 @@ final class NewsTableViewCell: UITableViewCell {
     }
     
     private func viewConfig() {
-        newsImageView.clipsToBounds = true
-        newsImageView.layer.cornerRadius = 10
+        [newsImageView, newsTitleLabel, newsDescriptionLabel].forEach {
+            contentView.addSubview($0)
+        }
         
-        newsTitleLabel.numberOfLines = 1
-        newsTitleLabel.textColor = .white
-        newsTitleLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        newsImageView.snp.makeConstraints { make in
+            make.leading.equalTo(contentView).offset(15)
+            make.width.equalTo(120)
+            make.centerY.equalToSuperview()
+            make.height.equalTo(newsImageView.snp.width).multipliedBy(2.0/3.0)
+        }
         
-        newsDescriptionLabel.numberOfLines = 3
-        newsDescriptionLabel.textColor = .systemGray2
-        newsDescriptionLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        newsTitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(newsImageView.snp.trailing).offset(15)
+            make.top.trailing.equalTo(contentView).inset(15)
+        }
+        newsTitleLabel.snp.contentHuggingVerticalPriority = 252
+        newsTitleLabel.snp.contentCompressionResistanceVerticalPriority = 752
         
-        newsImageInitialWidth = newsImageView.frame.width
-        newsImageInitialLeftConstant = newsImageViewLeftConstraint.constant        
+        newsDescriptionLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(newsTitleLabel)
+            make.top.bottom.equalTo(newsTitleLabel.snp.bottom).offset(10)
+            make.bottom.greaterThanOrEqualTo(contentView).offset(15)
+        }
     }
     
     func configure(with data: NewsData) {
-        newsImageViewWidthConstraint.constant = data.imageURL == nil ? 0 : newsImageInitialWidth
-        newsImageViewLeftConstraint.constant = data.imageURL == nil ? 0 : newsImageInitialLeftConstant
-        
+        let newsImageState: NewsImageState = data.imageURL == nil ? .isEmpty : .isExist
+        updateNewsImageViewConstarint(state: newsImageState)
         newsTitleLabel.text = data.title?.removeSearchTag
         newsDescriptionLabel.text = data.description?.removeSearchTag
         newsImageView.kf.setImage(with: URL(string: data.imageURL ?? ""))
     }
+    
+    private func updateNewsImageViewConstarint(state: NewsImageState) {
+        newsImageView.snp.updateConstraints { make in
+            make.width.equalTo(state.width)
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(contentView).offset(state.leftConstant)
+            make.height.equalTo(newsImageView.snp.width).multipliedBy(2.0/3.0)
+        }
+    }
 }
+
+extension NewsTableViewCell {
+    enum NewsImageState {
+        case isExist
+        case isEmpty
+        
+        var width: CGFloat {
+            switch self {
+            case .isEmpty: return 0
+            case .isExist: return 120
+            }
+        }
+        
+        var leftConstant: CGFloat {
+            switch self {
+            case .isEmpty: return 0
+            case .isExist: return 15
+            }
+        }
+    }
+}
+
