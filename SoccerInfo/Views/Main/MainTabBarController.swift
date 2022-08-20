3e2b0ce7559a94361f89ede9b8756c128644fc0b
JD-man <ekgkzmtm@gmail.com>
2022-08-21 00:45:20 +0900

    MainTabbarController Code-Based UI 수정

--- SoccerInfo/Views/Main/MainTabBarController.swift
+++ SoccerInfo/Views/Main/MainTabBarController.swift
@@ -12,22 +12,51 @@ final class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewConfig()        
+        viewConfig()
     }
     
     private func viewConfig() {
-        if #available(iOS 15.0, *) {
-            let tabbarAppearance = UITabBarAppearance()
-            tabbarAppearance.configureWithOpaqueBackground()
-            tabbarAppearance.backgroundColor = .black
-            
-            UITabBar.appearance().tintColor = .white
-            UITabBar.appearance().standardAppearance = tabbarAppearance
-            UITabBar.appearance().scrollEdgeAppearance = tabbarAppearance            
+        let viewControllers = TabbarItem.allCases.map { item -> UIViewController in
+            let vc = item.viewController
+            vc.tabBarItem.image = UIImage(systemName: item.tabbarImageName)
+            return vc
         }
-        else {
-            UITabBar.appearance().tintColor = .white
-            UITabBar.appearance().barTintColor = .black
+        setViewControllers(viewControllers, animated: false)
+    }
+}
+
+extension MainTabBarController {
+    
+    enum TabbarItem: CaseIterable {
+        case fixtures
+        case teamSchedules
+        case standings
+        case news
+        
+        var viewController: UIViewController {
+            switch self {
+            case .fixtures:
+                return FixturesViewController()
+            case .teamSchedules:
+                return TeamSchedulesViewController()
+            case .standings:
+                return StandingsViewController()
+            case .news:
+                return NewsViewController()
+            }
+        }
+        
+        var tabbarImageName: String {
+            switch self {
+            case .fixtures:
+                return "calendar"
+            case .teamSchedules:
+                return "puzzlepiece"
+            case .standings:
+                return "list.number"
+            case .news:
+                return "newspaper"
+            }
         }
     }
 }
