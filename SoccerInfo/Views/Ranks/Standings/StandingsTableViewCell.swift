75f9a3d3eac761d207dc0fad0c641eddbd07acf4
JD-man <ekgkzmtm@gmail.com>
2022-08-21 13:27:01 +0900

    순위테이블뷰 셀 초기화, UI 재설정

--- SoccerInfo/Views/Ranks/Standings/StandingsTableViewCell.swift
+++ SoccerInfo/Views/Ranks/Standings/StandingsTableViewCell.swift
@@ -8,28 +8,87 @@
 import UIKit
 
 final class StandingsTableViewCell: UITableViewCell {
-    @IBOutlet weak var rankLabel: UILabel!
-    @IBOutlet weak var teamNameLabel: UILabel!
-    @IBOutlet weak var pointsLabel: UILabel!
-    @IBOutlet weak var playedLabel: UILabel!
+    private let rankLabel = UILabel()
+    private let teamNameLabel = UILabel()
     
-    @IBOutlet weak var winLabel: UILabel!
-    @IBOutlet weak var drawLabel: UILabel!
-    @IBOutlet weak var loseLabel: UILabel!
-    @IBOutlet weak var goalDiffLabel: UILabel!
+    private let pointsLabel = UILabel()
+    private let playedLabel = UILabel()
+    private let winLabel = UILabel()
+    private let drawLabel = UILabel()
+    private let loseLabel = UILabel()
+    private let goalDiffLabel = UILabel()
     
-    @IBOutlet weak var separateLine: UIView!
+    private lazy var infoStackView: UIStackView = {
+        let labels = [pointsLabel, playedLabel, winLabel, drawLabel, loseLabel, goalDiffLabel]
+        let stackView = UIStackView(arrangedSubviews: labels)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    private let separateLine = UIView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         viewConfig()
+        configrationConfig()
     }
     
-    private func viewConfig() {        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func viewConfig() {
+        
+        [rankLabel, teamNameLabel, infoStackView, separateLine].forEach {
+            contentView.addSubview($0)
+        }
+        
+        [playedLabel, winLabel, drawLabel, loseLabel, goalDiffLabel].forEach {
+            $0.textAlignment = .center
+            $0.font = .systemFont(ofSize: 15, weight: .regular)
+            $0.textColor = .systemBackground
+        }
+        
+        zip([rankLabel, pointsLabel, teamNameLabel],
+            [UIFont.Weight.semibold, UIFont.Weight.semibold, UIFont.Weight.medium])
+        .forEach { label, weight in
+            label.font = .systemFont(ofSize: 15, weight: weight)
+            label.textAlignment = .center
+            label.textColor = .systemBackground
+        }
+        
         separateLine.isHidden = true
-        rankLabel.font = .systemFont(ofSize: 15, weight: .semibold)
-        pointsLabel.font = .systemFont(ofSize: 15, weight: .semibold)
-        backgroundColor = .clear        
+        separateLine.backgroundColor = .systemGray2
+        backgroundColor = .clear
+    }
+    
+    private func configrationConfig() {
+        rankLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(15)
+            make.width.equalToSuperview().multipliedBy(0.07)
+        }
+        
+        teamNameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(rankLabel.snp.trailing).offset(20)
+        }
+        
+        infoStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-15)
+            make.width.equalToSuperview().multipliedBy(0.5)
+            make.leading.equalTo(teamNameLabel.snp.trailing).offset(8)
+            make.height.equalTo(infoStackView.snp.width).multipliedBy(0.36)
+        }
+        
+        separateLine.snp.makeConstraints { make in
+            make.height.equalTo(1.5)
+            make.bottom.equalToSuperview()
+            make.leading.equalTo(rankLabel)
+            make.trailing.equalTo(infoStackView)
+        }
     }
     
     func configure(with data: StandingsRealmData) {
