2352a53c6f5d9149e4d773614819738f41eb77b9
JD-man <ekgkzmtm@gmail.com>
2022-08-21 15:19:57 +0900

    팀일정 테이블뷰 셀 초기화, UI 재설정

--- SoccerInfo/Views/TeamSchedules/TeamSchedulesTableViewCell.swift
+++ SoccerInfo/Views/TeamSchedules/TeamSchedulesTableViewCell.swift
@@ -7,30 +7,50 @@
 
 import UIKit
 
-class TeamSchedulesTableViewCell: UITableViewCell {
-
+final class TeamSchedulesTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var versusLabel: UILabel!
-    @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var homeAwayLabel: UILabel!
-    @IBOutlet weak var scheduleDateLabel: UILabel!
+    private let versusLabel = TeamSchedulesLabel(fontSize: 16, fontWeight: .semibold, alignment: .right)
+    private let resultLabel = TeamSchedulesLabel(fontSize: 16, fontWeight: .semibold, alignment: .right)
+    private let homeAwayLabel = TeamSchedulesLabel(fontSize: 16, fontWeight: .semibold, alignment: .right)
+    private let scheduleDateLabel = TeamSchedulesLabel(fontSize: 18, fontWeight: .bold, alignment: .left)
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         viewConfig()
+        constraintsConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func viewConfig() {
-        [versusLabel, resultLabel, homeAwayLabel]
-            .forEach {
-                $0?.text = ""
-                $0?.textColor = .white
-                $0?.textAlignment = .right
-                $0?.font = .systemFont(ofSize: 16, weight: .semibold)
-            }
-        scheduleDateLabel.text = ""
-        scheduleDateLabel.textColor = .white
-        scheduleDateLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        [scheduleDateLabel, versusLabel, homeAwayLabel, resultLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func constraintsConfig() {
+        scheduleDateLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        versusLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(scheduleDateLabel)
+            make.top.equalTo(scheduleDateLabel.snp.bottom).offset(10)
+        }
+        
+        homeAwayLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(versusLabel)
+            make.top.equalTo(versusLabel.snp.bottom).offset(10)
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(versusLabel)
+            make.top.equalTo(homeAwayLabel.snp.bottom).offset(10)
+            make.bottom.equalToSuperview().offset(-10)
+        }
     }
     
     func configure(with data: TeamScheduleCellModel) {
