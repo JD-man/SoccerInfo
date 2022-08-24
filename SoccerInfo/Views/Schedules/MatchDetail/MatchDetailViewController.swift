7a209eda7b6109a589144aa12a77757b829bbab5
JD-man <ekgkzmtm@gmail.com>
2022-08-24 10:56:16 +0900

    포메이션 테이블뷰 셀 등록 재설정

--- SoccerInfo/Views/Schedules/MatchDetail/MatchDetailViewController.swift
+++ SoccerInfo/Views/Schedules/MatchDetail/MatchDetailViewController.swift
@@ -52,20 +52,12 @@ final class MatchDetailViewController: UIViewController {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.separatorStyle = .none
         // 각 셀들을 다시 만들면서 주석처리한걸 정리하면 됨!
-        
         tableView.register(EventsTableViewCell.self,
                            forCellReuseIdentifier: EventsTableViewCell.identifier)
         tableView.register(LineupsTableViewCell.self,
                            forCellReuseIdentifier: LineupsTableViewCell.identifier)
-//        tableView.register(FormationTableViewCell.self,
-//                           forCellReuseIdentifier: FormationTableViewCell.identifier)
-        //let eventsCell = UINib(nibName: EventsTableViewCell.identifier, bundle: nil)
-        //let lineupsCell = UINib(nibName: LineupsTableViewCell.identifier, bundle: nil)
-        let formationCell = UINib(nibName: FormationTableViewCell.identifier, bundle: nil)
-//        tableView.register(eventsCell, forCellReuseIdentifier: EventsTableViewCell.identifier)
-//        tableView.register(lineupsCell, forCellReuseIdentifier: LineupsTableViewCell.identifier)
-        tableView.register(formationCell, forCellReuseIdentifier: FormationTableViewCell.identifier)
-
+        tableView.register(FormationTableViewCell.self,
+                           forCellReuseIdentifier: FormationTableViewCell.identifier)
         return tableView
     }()
     
