666071b1def925885b667a577325e0f7318414b8
JD-man <ekgkzmtm@gmail.com>
2022-11-29 17:38:49 +0900

    component 수정, mainBuilder 추가

--- Targets/FootballInfo/Sources/RIBs/Root/RootBuilder.swift
+++ Targets/FootballInfo/Sources/RIBs/Root/RootBuilder.swift
@@ -42,11 +42,19 @@ final class RootBuilder: Builder<RootDependency>, RootBuildable {
   
   func build() -> LaunchRouting {
     let viewController = RootViewController()
+    
+    let component = RootComponent(
+      dependency: dependency,
+      rootViewController: viewController
+    )
+    
     let interactor = RootInteractor(presenter: viewController)
+    let mainBuilder = MainBuilder(dependency: component)
     
     return RootRouter(
       interactor: interactor,
-      viewController: viewController
+      viewController: viewController,
+      mainBuilder: mainBuilder
     )
   }
 }
