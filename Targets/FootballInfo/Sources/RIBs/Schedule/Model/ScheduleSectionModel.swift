697a457ce69d463ce17bc02d8289047ec41fd42c
JD-man <ekgkzmtm@gmail.com>
2022-12-02 18:00:41 +0900

    Schedule TableView RxDataSources Section Model 추가

---
+++ Targets/FootballInfo/Sources/RIBs/Schedule/Model/ScheduleSectionModel.swift
@@ -0,0 +1,56 @@
+//
+//  ScheduleSectionModel.swift
+//  FootballInfo
+//
+//  Created by 조동현 on 2022/12/02.
+//
+
+import Foundation
+import RxDataSources
+
+struct ScheduleSectionModel: AnimatableSectionModelType {
+  typealias Identity = String
+  
+  var identity: String { return dateHeader }
+  
+  var dateHeader: String
+  var items: [Item]
+}
+
+extension ScheduleSectionModel {
+  init(original: ScheduleSectionModel, items: [Item]) {
+    self = original
+    self.items = items
+  }
+}
+
+extension ScheduleSectionModel {
+  struct Item: IdentifiableType, Equatable {
+    typealias Identity = Int
+    var identity: Int { fixtureID }
+    
+    let homeID: Int
+    let homeName: String
+    let homeLogo: String
+    var homeGoal: Int?
+    
+    let awayID: Int
+    let awayName: String
+    let awayLogo: String
+    var awayGoal: Int?
+    
+    let matchHour: String
+    let fixtureID: Int
+    
+    static func emptyContent(id: Int) -> Item {
+      return .init(homeID: 0,
+                   homeName: "empty",
+                   homeLogo: "",
+                   awayID: 0,
+                   awayName: "",
+                   awayLogo: "",
+                   matchHour: "",
+                   fixtureID: id)
+    }
+  }
+}
