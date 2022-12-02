6a15ae69372341d53b73d67b0a414f40d19a9fed
JD-man <ekgkzmtm@gmail.com>
2022-12-03 00:18:53 +0900

    ScheduleReactorModel leagueInfo 추가

--- Targets/FootballInfo/Sources/RIBs/Schedule/Model/ScheduleReactorModel.swift
+++ Targets/FootballInfo/Sources/RIBs/Schedule/Model/ScheduleReactorModel.swift
@@ -13,16 +13,19 @@ struct ScheduleReactorModel {
     case fetchSchedule
     case prevSchedule
     case nextSchedule
+    case changeLeague(LeagueInfo.League)
   }
   
   enum Mutation {
     case setFirstDay(Date)
+    case setLeagueInfo(LeagueInfo)
     case setTotalScheduleDictionary(TotalScheduleDictionary)
     case setWeeklyScheduleContent([ScheduleSectionModel])
   }
   
   struct State {
     var firstDay: Date
+    var leagueInfo: LeagueInfo = LeagueInfo(league: .premierLeague)
     var totalScheduleDictionary: TotalScheduleDictionary = [:]
     var weeklyScheduleContent: [ScheduleSectionModel] = []
   }
