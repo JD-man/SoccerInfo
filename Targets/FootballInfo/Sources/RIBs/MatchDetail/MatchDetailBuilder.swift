b3002d33f685cc79879f73d3b738e0741e1863ea
JD-man <ekgkzmtm@gmail.com>
2022-12-04 18:59:13 +0900

    MatchDetail RIBs 파일 추가
    기존 MatchDetailViewController 이름 변경 포함

---
+++ Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailBuilder.swift
@@ -0,0 +1,39 @@
+//
+//  MatchDetailBuilder.swift
+//  FootballInfo
+//
+//  Created by 조동현 on 2022/12/04.
+//
+
+import RIBs
+
+protocol MatchDetailDependency: Dependency {
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
+}
+
+final class MatchDetailComponent: Component<MatchDetailDependency> {
+
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol MatchDetailBuildable: Buildable {
+    func build(withListener listener: MatchDetailListener) -> MatchDetailRouting
+}
+
+final class MatchDetailBuilder: Builder<MatchDetailDependency>, MatchDetailBuildable {
+
+    override init(dependency: MatchDetailDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: MatchDetailListener) -> MatchDetailRouting {
+        let component = MatchDetailComponent(dependency: dependency)
+        let viewController = MatchDetailViewController()
+        let interactor = MatchDetailInteractor(presenter: viewController)
+        interactor.listener = listener
+        return MatchDetailRouter(interactor: interactor, viewController: viewController)
+    }
+}
---
+++ Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MatchDetailViewController.swift
+//  FootballInfo
+//
+//  Created by 조동현 on 2022/12/04.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol MatchDetailPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class MatchDetailViewController: UIViewController, MatchDetailPresentable, MatchDetailViewControllable {
+
+    weak var listener: MatchDetailPresentableListener?
+}
