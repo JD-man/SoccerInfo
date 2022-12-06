c3c87107c04b1cad519aa22775e1b2150c59ee22
JD-man <ekgkzmtm@gmail.com>
2022-12-06 11:57:14 +0900

    MatchDetailViewController 초기 UI 설정

--- Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailViewController.swift
+++ Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailViewController.swift
@@ -18,4 +18,48 @@ protocol MatchDetailPresentableListener: AnyObject {
 final class MatchDetailViewController: UIViewController, MatchDetailPresentable, MatchDetailViewControllable {
   
   weak var listener: MatchDetailPresentableListener?
+  
+  private var activityView = UIActivityIndicatorView()
+  private let headerView = MatchDetailTableHeaderView()
+  
+  private lazy var gradient: CAGradientLayer = {
+    let gradient = CAGradientLayer()
+    gradient.frame = view.bounds
+    gradient.locations = [0.0, 1.0]
+    gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+    gradient.startPoint = CGPoint(x: 0.5, y: 0.7)
+    return gradient
+  }()
+  
+  private let matchDetailTableView: UITableView = {
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    tableView.separatorStyle = .none
+    tableView.backgroundColor = .clear
+    tableView.register(EventsTableViewCell.self,
+                       forCellReuseIdentifier: EventsTableViewCell.identifier)
+    tableView.register(LineupsTableViewCell.self,
+                       forCellReuseIdentifier: LineupsTableViewCell.identifier)
+    tableView.register(FormationTableViewCell.self,
+                       forCellReuseIdentifier: FormationTableViewCell.identifier)
+    return tableView
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    viewConfig()
+    constraintsConfig()
+  }
+  
+  private func viewConfig() {
+    title = "경기정보"
+    view.addSubview(matchDetailTableView)
+    view.layer.insertSublayer(gradient, at: 0)
+    matchDetailTableView.tableHeaderView = headerView
+  }
+  
+  private func constraintsConfig() {
+    matchDetailTableView.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide).inset(15)
+    }
+  }
 }
