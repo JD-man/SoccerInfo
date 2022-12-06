fcfd5040e5865646730cb8807610766da3a2d79e
JD-man <ekgkzmtm@gmail.com>
2022-12-06 19:48:52 +0900

    ScheduleRouter MatchDetail RIB 라우팅

--- Targets/FootballInfo/Sources/RIBs/Schedule/ScheduleRouter.swift
+++ Targets/FootballInfo/Sources/RIBs/Schedule/ScheduleRouter.swift
@@ -7,20 +7,55 @@
 
 import RIBs
 
-protocol ScheduleInteractable: Interactable {
+protocol ScheduleInteractable: Interactable, MatchDetailListener {
   var router: ScheduleRouting? { get set }
   var listener: ScheduleListener? { get set }
 }
 
 protocol ScheduleViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+  func pushMatchDetail(_ viewControllable: ViewControllable)
+  func popMatchDetail()
 }
 
 final class ScheduleRouter: ViewableRouter<ScheduleInteractable, ScheduleViewControllable>, ScheduleRouting {
   
+  private let matchDetailBuilder: MatchDetailBuildable
+  private var matchDetailRouter: MatchDetailRouting?
+  
   // TODO: Constructor inject child builder protocols to allow building children.
-  override init(interactor: ScheduleInteractable, viewController: ScheduleViewControllable) {
+  init(
+    interactor: ScheduleInteractable,
+    viewController: ScheduleViewControllable,
+    matchDetailBuilder: MatchDetailBuildable)
+  {
+    self.matchDetailBuilder = matchDetailBuilder
     super.init(interactor: interactor, viewController: viewController)
     interactor.router = self
+  }
+}
+
+// MARK: - MatchDetail Routing
+extension ScheduleRouter {
+  func attachMatchDetail(fixtureId: Int, leagueInfo: LeagueInfo, headerModel: MatchDetailHeaderModel) {
+    guard matchDetailRouter == nil else { return }
+    
+    let matchDetailRouter = matchDetailBuilder.build(
+      withListener: interactor,
+      fixtureId: fixtureId,
+      leagueInfo: leagueInfo,
+      headerModel: headerModel
+    )
+    
+    self.matchDetailRouter = matchDetailRouter
+    viewController.pushMatchDetail(matchDetailRouter.viewControllable)
+    attachChild(matchDetailRouter)
+  }
+  
+  func detachMatchDetail() {
+    print("detach match Detail")
+  }
+  
+  func presentNotiAlert() {
+    print("present noti alert")
   }
 }
