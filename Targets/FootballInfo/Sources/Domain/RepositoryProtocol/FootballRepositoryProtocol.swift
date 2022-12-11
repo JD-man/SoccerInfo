b0b86a6762a8e35dbc92fad695b47d6668c9575d
JD-man <ekgkzmtm@gmail.com>
2022-12-11 13:46:31 +0900

    footballRepository event, lineup 구현
    footballRepository 스위프트 파일에 프롤토콜 삭제, 오타 수정

--- Targets/FootballInfo/Sources/Domain/RepositoryProtocol/FootballRepositoryProtocol.swift
+++ Targets/FootballInfo/Sources/Domain/RepositoryProtocol/FootballRepositoryProtocol.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol FootballRepositoryProtocol: AnyObject {
+  func fixture(season: Int, league: String) -> Observable<FixturesAPIData>
+  func event(fixtureId: String) -> Observable<EventsAPIData>
+  func lineup(fixtureId: String) -> Observable<LineupsAPIData>
+}
