55f14b8bd521158e09d30ba9449a1fa072a7f28d
JD-man <ekgkzmtm@gmail.com>
2022-12-11 13:59:19 +0900

    MatchDetailInteractor SectionModel 생성 구현

--- Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailInteractor.swift
+++ Targets/FootballInfo/Sources/RIBs/MatchDetail/MatchDetailInteractor.swift
@@ -36,6 +36,7 @@ final class MatchDetailInteractor: PresentableInteractor<MatchDetailPresentable>
   weak var listener: MatchDetailListener?
   
   var initialState: State
+  private let useCase: MatchDetailUseCaseProtocol
   
   // TODO: Add additional dependencies to constructor. Do not perform any logic
   // in constructor.
@@ -43,7 +44,10 @@ final class MatchDetailInteractor: PresentableInteractor<MatchDetailPresentable>
     presenter: MatchDetailPresentable,
     fixtureId: Int,
     leagueInfo: LeagueInfo,
-    headerModel: MatchDetailHeaderModel) {
+    headerModel: MatchDetailHeaderModel,
+    useCase: MatchDetailUseCaseProtocol
+  ) {
+    self.useCase = useCase
     self.initialState = State(fixtureId: fixtureId, leagueInfo: leagueInfo, headerModel: headerModel)
     super.init(presenter: presenter)
     presenter.listener = self
@@ -63,16 +67,70 @@ final class MatchDetailInteractor: PresentableInteractor<MatchDetailPresentable>
 extension MatchDetailInteractor: Reactor {
   func mutate(action: MatchDetailReactorModel.Action) -> Observable<MatchDetailReactorModel.Mutation> {
     switch action {
-      
+    case .fetchMatchDetail:
+      let fixtureId = currentState.fixtureId
+      let season = currentState.leagueInfo.season
+      let league = currentState.leagueInfo.league
+      return useCase.executeRealmMatchDetail(season: season, league: league.rawValue, fixtureId: fixtureId)
+        .withUnretained(self)
+        .flatMap { interactor, matchDetailEntity -> Observable<MatchDetailReactorModel.Mutation> in
+          let eventSection = interactor.configureEventSection(of: matchDetailEntity)
+          let formationSection = interactor.configureFormationSection(of: matchDetailEntity)
+          let startingLineupSection = interactor.configureStartingLineupSectionItem(of: matchDetailEntity)
+          let benchLineupSection = interactor.configureBenchLineupSectionItem(of: matchDetailEntity)
+          return .just(.setMatchDetailSection([eventSection, formationSection, startingLineupSection, benchLineupSection]))
+        }
     }
   }
   
   func reduce(state: MatchDetailReactorModel.State, mutation: MatchDetailReactorModel.Mutation) -> MatchDetailReactorModel.State {
     var newState = state
     switch mutation {
-      
+    case .setMatchDetailSection(let section):
+      newState.matchDetailSection = section
     }
     return newState
   }
   
+}
+
+// MARK: - Match Detail TableView Section
+extension MatchDetailInteractor {
+  private func configureEventSection(of matchDetailEntity: MatchDetailEntity) -> MatchDetailSectionModel {
+    let homeTeamName = currentState.headerModel.homeTeamName
+    let sectionItem = matchDetailEntity.events.map {
+      let isHomeCell = $0.teamName == homeTeamName
+      return MatchDetailSectionModel.Item.eventCell($0, isHomeCell)
+    }
+    return MatchDetailSectionModel(section: .event, items: sectionItem)
+  }
+  
+  private func configureFormationSection(of matchDetailEntity: MatchDetailEntity) -> MatchDetailSectionModel {
+    let homeLineup = matchDetailEntity.homeStartLineup
+    let awayLineup = matchDetailEntity.awayStartLineup
+    let sectionItem = [MatchDetailSectionModel.Item.formationCell(homeLineup, awayLineup)]
+    return MatchDetailSectionModel(section: .formation, items: sectionItem)
+  }
+  
+  private func configureStartingLineupSectionItem(of matchDetailEntity: MatchDetailEntity) -> MatchDetailSectionModel {
+    let homeLineup = matchDetailEntity.homeStartLineup
+    let awayLineup = matchDetailEntity.awayStartLineup
+    let leagueInfo = currentState.leagueInfo
+    
+    let sectionItem = zip(homeLineup, awayLineup).map { homePlayer, awayPlayer in
+      return MatchDetailSectionModel.Item.startingLineupCell(homePlayer, awayPlayer, leagueInfo)
+    }
+    return MatchDetailSectionModel(section: .startingLineup, items: sectionItem)
+  }
+  
+  private func configureBenchLineupSectionItem(of matchDetailEntity: MatchDetailEntity) -> MatchDetailSectionModel {
+    let homeLineup = matchDetailEntity.homeSubLineup
+    let awayLineup = matchDetailEntity.awaySubLineup
+    let leagueInfo = currentState.leagueInfo
+    
+    let sectionItem = zip(homeLineup, awayLineup).map { homePlayer, awayPlayer in
+      return MatchDetailSectionModel.Item.startingLineupCell(homePlayer, awayPlayer, leagueInfo)
+    }
+    return MatchDetailSectionModel(section: .benchLineup, items: sectionItem)
+  }
 }
