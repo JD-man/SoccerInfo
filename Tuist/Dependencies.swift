60ff715c2ab6939551c16edc7c8c9370fb0630d3
JD-man <ekgkzmtm@gmail.com>
2023-06-11 15:18:34 +0900

    SPM 킹피셔 및 GA 버전 업데이트

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -24,13 +24,13 @@ let dependencies = Dependencies(
       .remote(url: "https://github.com/SnapKit/SnapKit.git",
               requirement: .upToNextMajor(from: "5.6.0")),
       .remote(url: "https://github.com/onevcat/Kingfisher.git",
-              requirement: .upToNextMajor(from: "7.4.0")),
+              requirement: .upToNextMajor(from: "7.6.2")),
       .remote(url: "https://github.com/jonkykong/SideMenu.git",
               requirement: .upToNextMajor(from: "6.5.0")),
       .remote(url: "https://github.com/danielgindi/Charts.git",
               requirement: .upToNextMajor(from: "4.1.0")),
       .remote(url: "https://github.com/firebase/firebase-ios-sdk.git",
-              requirement: .upToNextMajor(from: "8.0.0"))
+              requirement: .upToNextMajor(from: "10.0.0"))
     ],
     platforms: [.iOS]
 )
